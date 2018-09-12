bdab94b319f50d5235ae5110c4920862d75ee911
lalamiko <36137409+lalamiko@users.noreply.github.com>
2018-09-12 18:03:05 +0200

    17:54

--- DiscordBotCore/Program.cs
+++ DiscordBotCore/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace DiscordBotCore
 {
     internal class Program
     {
-        private static void Main()
+        private static async Task Main()
         {
             Unity.RegisterTypes();
             Console.WriteLine("Hello, Discord!");
+            await MyWeirdTask();
+        }
+
+        private static async Task MyWeirdTask()
+        {
+            Console.WriteLine("Oh yea, there it is.");
+            await Task.Delay(-1);
         }
     }
 }
